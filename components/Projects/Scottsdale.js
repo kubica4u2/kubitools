3cc3a9efcbbaf754467dfc9cc1789c317911eb25
James Kubica <kubica4u2@gmail.com>
2022-10-12 23:45:40 -0500

    added ref attr to link

--- components/Projects/Scottsdale.js
+++ components/Projects/Scottsdale.js
@@ -4,7 +4,7 @@ const Scottsdale = ({obj}) => {
         <ProjectWrapper>
             <div className="flex justify-between items-center py-4">
                 <h1 className="text-white text-lg md:text-3xl font-bold uppercase">{obj.name}</h1>
-                <a href={obj.url} className="text-white" target="_blank">View Website</a>
+                <a href={obj.url} className="text-white" rel="noreferrer" target="_blank">View Website</a>
             </div>
             <div className="relative mx-auto sm:max-w-3xl sm:px-6 lg:max-w-none lg:px-0 lg:pt-4">
                 <div className="relative overflow-hidden rounded-2xl pt-64 pb-10 shadow-xl">
