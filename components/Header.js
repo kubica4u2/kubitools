ace568050927724f99b04bb84fa03b2e6c966955
kubica4u2 <kubica4u2@gmail.com>
2024-02-18 17:21:30 -0600

    Fix - Email

--- components/Header.js
+++ components/Header.js
@@ -81,7 +81,7 @@ const Header = () => {
                                     </button>
                                 </a>
 
-                                <a href="mailto:kubitools.com">
+                                <a href="mailto:james@kubitools.com">
                                     <button
                                         type="button"
                                         className="rounded-full bg-gray-800 p-1 text-white hover:text-color-1 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
